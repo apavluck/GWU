7bb7cb8c5cc1376923d0e73f54ebf09ce8a49972
Alex Pavluck <apavluck@rti.org>
2016-02-07 08:43:33 -0500

    Just working through some examples in the textbook

--- scratch_pad.py
+++ scratch_pad.py
@@ -1,3 +1,5 @@
+'''
+
 #fibonacci
 def fib (x):
     a = 0
@@ -25,3 +27,30 @@ for items in k:
         print items, z
         j = False
 print j
+
+'''
+
+def breakitintoletters(name):
+    for char in name:
+        print char
+
+
+word = "Mississippi"
+count = 0
+
+for letter in word:
+    if letter == "s":
+        count +=1
+
+print count
+
+
+sentence = "This is a test program to see if I have the skills needed to do this!"
+
+key = "program"
+word = "is"
+
+if word < key:
+    print "your word is before program"
+elif word > key:
+    print "your word is after program"
